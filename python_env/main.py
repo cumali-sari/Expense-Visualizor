6d4f5ca75e3a57b71eefcc4e6927d46d6ca988b4
cumali-sari <sariczx@gmail.com>
2025-11-26 07:44:21 +0300

    Add header detector

--- python_env/main.py
+++ python_env/main.py
@@ -1,11 +1,33 @@
 
 import pandas as pd
 
-df= pd.read_excel("read.xlsx")
+df_raw = pd.read_excel("read.xlsx", header=None)
 
-df= df.dropna() # drop rows that has at least one missing value
+# drop rows that has at least one missing value
+# by that get rid of lines before and after dataframe
+df_raw= df_raw.dropna() 
+
+
+# move header to df's columns
+
+header_row = df_raw.apply(
+    lambda row: row.astype(str).str.isalpha().any(),
+    axis=1
+).idxmax()
+
+df = pd.read_excel("read.xlsx", header=header_row)
+
+df= df.dropna()
+#print(df["Tarih"])
+
+
+print(df)
 
 
 
-print(df.to_string)
+
+
+
+
+
 
